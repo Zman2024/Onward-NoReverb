e10051db22def66a8b2ec10f5054d6fc4e56b65b
Zman2024 <minertlink5@gmail.com>
2022-11-28 18:29:38 -0700

    Add project files.

---
+++ NoReverb/HarmonyPatches.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using UnityEngine;
+
+namespace NoReverb
+{
+    [HarmonyPatch]
+    public static class HarmonyPatches
+    {
+        public static BepInEx.Logging.ManualLogSource Logger;
+
+        // AreaReverb is the cause of all spot/spatial reverb
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(AreaReverb), "Start")]
+        public static bool AreaReverb_Start(AreaReverb __instance)
+        {
+            if (Configuration.DisableAreaReverb)
+            {
+                Logger.LogDebug("Disabling AreaReverb instance");
+                __instance.enabled = false;
+                return false;
+            }
+            return true;
+        }
+        
+        // This is basically the reverb for everything that isn't a gunshot
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(AudioManager), nameof(AudioManager.GetMixerFromPreset))]
+        public static void GetMixerFromPreset(object[] __args)
+        {
+            if (Configuration.DisableOtherReverb)
+            {
+                Logger.LogDebug("Forcing environment reverb to AudioReverbPreset.Off");
+                __args[0] = AudioReverbPreset.Off;
+            }
+        }
+
+        // Reverb for gunshots
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(AudioManager), nameof(AudioManager.GetGunshotMixerFromPreset))]
+        public static void GetGunshotMixerFromPreset_NoReverb(object[] __args)
+        {
+            if (Configuration.DisableGunshotReverb)
+            {
+                Logger.LogDebug("Forcing gunshot reverb to AudioReverbPreset.Off");
+                __args[0] = AudioReverbPreset.Off;
+            }
+        }
+
+    }
+}
---
+++ NoReverb/Plugin.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using BepInEx;
+using BepInEx.Configuration;
+
+namespace NoReverb
+{
+    public static class Configuration
+    {
+        private static ConfigFile _config = null;
+
+        public static bool DisableAreaReverb { get; private set; } = true;
+
+        public static bool DisableGunshotReverb { get; private set; } = true;
+
+        public static bool DisableOtherReverb { get; private set; } = true;
+
+        // Config definitions //
+        private static readonly string ConfigSection = "NoReverb";
+        private static readonly ConfigDefinition areaReverb = new ConfigDefinition(ConfigSection, "DisableAreaReverb");
+        private static readonly ConfigDefinition gunshotReverb = new ConfigDefinition(ConfigSection, "DisableGunshotReverb");
+        private static readonly ConfigDefinition otherReverb = new ConfigDefinition(ConfigSection, "DisableOtherReverb");
+
+        public static void LoadConfig(ConfigFile cfg)
+        {
+            _config = cfg;
+            if (_config.Count == 0)
+            {
+                CreateConfig(cfg);
+                return;
+            }
+
+            ConfigReloaded(null, null);
+            _config.ConfigReloaded += ConfigReloaded;
+        }
+
+        private static void ConfigReloaded(object sender, EventArgs e)
+        {
+            DisableAreaReverb = (bool)_config[areaReverb].BoxedValue;
+            DisableGunshotReverb = (bool)_config[gunshotReverb].BoxedValue;
+            DisableOtherReverb = (bool)_config[otherReverb].BoxedValue;
+        }
+
+        public static void CreateConfig(ConfigFile cfg)
+        {
+            cfg.Bind(areaReverb, true, new ConfigDescription("Disables the AreaReverb script from running, stopping reverb in areas that it was explicitly added in"));
+            cfg.Bind(gunshotReverb, true, new ConfigDescription("Disables reverb on gunshots"));
+            cfg.Bind(otherReverb, true, new ConfigDescription("Disables reverb on everything that isn't a gunshot"));
+        }
+
+    }
+
+    [BepInPlugin("Zman2024-NoReverb", "No Reverb", "0.1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        public void Start()
+        {
+            HarmonyPatches.Logger = this.Logger;
+            try
+            {
+                Logger.LogInfo("Loading config...");
+                Configuration.LoadConfig(Config);
+
+                Logger.LogInfo("Applying harmony patches...");
+                Harmony.CreateAndPatchAll(typeof(HarmonyPatches));
+                
+                Logger.LogInfo("Finished");
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(ex);
+                return;
+            }
+        }
+
+    }
+}
